18110d8155831a20f239d772c6ecbed08adba6f6
Ratikanta <ratikantamohanty924@gmail.com>
2025-11-12 20:15:43 +0530

    added authentication

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../Controllers/authControllers');
 const wrapAsync = require('../utills/wrapAsync');
-
+const verifyToken = require('../middlewares/authMiddleware');
 
 router.post("/register", wrapAsync(authController.register));
 router.get("/verify/:token", wrapAsync(authController.verifyEmail));
+router.post("/login",wrapAsync(authController.login));
+router.get("/protected", verifyToken , wrapAsync(authController.protected));
 
 
 module.exports = router;
